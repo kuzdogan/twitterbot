ba619b1f03903c5910442ef7f09e3f9a386e2829
kuzdogan <kaanuzdogan@hotmail.com>
2019-10-02 22:11:19 +0200

    Get a random unposted scene in intervals

--- app.js
+++ app.js
@@ -5,22 +5,43 @@ const path = require('path');
 const fs = require('fs');
 const mongoose = require('mongoose');
 const T = new Twitter(config);
-import SceneModel from './models/Scene';
+const SceneModel = require('./models/Scene').SceneModel;
 
+const DB_NAME = 'test';
 const IMAGE_PATH = path.join(__dirname, 'assets', 'images');
-const DB_URL = 'mongodb://localhost/test';
+const DB_URL = 'mongodb://localhost/' + DB_NAME;
 
 
 // Connect to db
-mongoose.connect(DB_URL, {useNewUrlParser: true});
-var db = mongoose.test;
+mongoose.connect(DB_URL, {useNewUrlParser: true, connectTimeoutMS: 5000});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected!")
+  // we're connected!
+});
 
-SceneModel.find({}).then((docs) => {
-  console.log(docs)
-})
+setInterval( () => {
+  getRandomScene().then( (scene) => {
+    console.log(scene);
+  });
+}, 5 * 1000)
 
-var scene = db.scenes.findOne();
-console.log(scene);
+
+/**
+ * @function to get a random scene
+ * @returns {Promise} with the Scene found
+ */
+function getRandomScene() {
+  return SceneModel.countDocuments().then( (count) => {
+    // Get a random entry
+    var random = Math.floor(Math.random() * count)
+    console.log('Random: ' + random);
+    return SceneModel.findOne({posted: false}).skip(random);
+  });
+}
+
+
 // Make post request on media endpoint. Pass file data as media parameter
 // T.post('media/upload', {media: image}, function(error, media, response) {
 
